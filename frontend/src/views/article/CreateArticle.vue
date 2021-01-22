df8220d81d544c8b0ece57a4d2eb52be2cfcd05e
shindonghyun <donghyun5349@naver.com>
2021-01-22 16:30:12 +0900

    S04P12B105-29 [front] feat: 게시물 작성 추가

---
+++ frontend/src/views/article/CreateArticle.vue
@@ -0,0 +1,107 @@
+<template>
+  <v-row
+    justify="center"
+  >
+    <v-col
+        lg="4"
+        md="4"
+        sm="6"
+    >
+      <h1>게시글 작성</h1>
+
+      <validation-observer ref="observer" v-slot="{ invalid }">
+        <form @submit.prevent="submit">
+          <validation-provider rules="required" v-slot="{ errors }">
+            <v-file-input
+              accept="image/*"
+              placeholder="추억을 올려주세요!"   
+              prepend-icon="mdi-camera"
+              :error-messages="errors"
+              @change="previewImg"
+              >
+            </v-file-input>
+            <v-img 
+              v-if='imagePreviewURL' 
+              :src=imagePreviewURL
+              max-height="150"
+              max-width="250">
+            </v-img>
+
+          </validation-provider>
+
+          <p>위치설정</p>
+          <div id="map" style="width:500px;height:400px;"></div>
+
+
+          <validation-provider rules="ruquired" v-slot="{ errors }">
+            <v-textarea
+              placeholder="추억을 적어주세요!"
+              type="text"
+              label="게시글"
+              :error-messages="errors"
+              v-model="content"
+              >
+              
+            </v-textarea>
+          </validation-provider>
+          <p>해시태그 자리</p>
+
+        
+          <v-btn
+            class="mr-4"
+            type="submit"
+            :disabled="invalid"
+            block
+          >
+            게시물 작성
+          </v-btn>
+        </form>
+      </validation-observer>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import axios from 'axios'
+import firebase from 'firebase/app'
+import { required } from 'vee-validate/dist/rules'
+import { extend, ValidationObserver, ValidationProvider, setInteractionMode } from 'vee-validate'
+
+const SERVER_URL = process.env.VUE_APP_SERVER_URL
+
+// https://logaretm.github.io/vee-validate/guide/interaction-and-ux.html#interaction-modes
+setInteractionMode('eager') // 유효성 검사의 시기
+
+extend('required', {
+  ...required,
+  message: '필수 입력 항목입니다.'
+})
+
+export default {
+  components: {
+    ValidationProvider,
+    ValidationObserver
+  },
+  data: () => {
+    return {
+      imagePreviewURL: 'null',
+      articleInfo: {
+        content: ''
+      }
+    }
+  },
+  methods: {
+    previewImg(res) {
+      const file = res.name
+      this.imagePreviewURL = URL.createObjectURL(res)
+    },
+    submit() {
+
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
