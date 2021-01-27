095348c7452eca4fc22de4d7cd7e0662a2f5f497
jongseong kim <rlawhdtjd9@naver.com>
2021-01-27 10:43:27 +0900

    S04P12B105-115 [back] fix: 팔로우 페이징 버그픽스

--- backend/src/main/java/com/web/curation/controller/user/UserController.java
+++ backend/src/main/java/com/web/curation/controller/user/UserController.java
@@ -1,6 +1,7 @@
 package com.web.curation.controller.user;
 
 import com.web.curation.commons.ErrorResponse;
+import com.web.curation.commons.PageRequest;
 import com.web.curation.dto.user.SimpleUserInfoDto;
 import com.web.curation.dto.user.UserPageDto;
 import com.web.curation.exceptions.UserNotFoundException;
@@ -8,11 +9,14 @@ import com.web.curation.security.CustomUserDetails;
 import com.web.curation.service.user.FollowService;
 import com.web.curation.service.user.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
@@ -36,18 +40,18 @@ public class UserController {
      */
 
     @GetMapping("/{userId}/followings")
-    public ResponseEntity<?> getFollowings(@PathVariable("userId") String userId, Pageable pageable, Authentication authentication){
+    public ResponseEntity<?> getFollowings(@PathVariable("userId") String userId, PageRequest pageable, Authentication authentication){
         final String currentUserId = ((CustomUserDetails)authentication.getPrincipal()).getUsername();
-        List<SimpleUserInfoDto> result = followService.findFollowingsByUserId(currentUserId, userId, pageable);
+        List<SimpleUserInfoDto> result = followService.findFollowingsByUserId(currentUserId, userId, pageable.of());
 
-        return ResponseEntity.ok().body(result);
+        return ResponseEntity.ok().body(new PageImpl<SimpleUserInfoDto>(result));
     }
 
     @GetMapping("/{userId}/followers")
-    public ResponseEntity<?> getFollowers(@PathVariable("userId") String userId, Pageable pageable, Authentication authentication){
+    public ResponseEntity<?> getFollowers(@PathVariable("userId") String userId, PageRequest pageable, Authentication authentication){
         final String currentUserId = ((CustomUserDetails)authentication.getPrincipal()).getUsername();
-        List<SimpleUserInfoDto> result = followService.findFollowersByUserId(currentUserId, userId, pageable);
-        return ResponseEntity.ok().body(result);
+        List<SimpleUserInfoDto> result = followService.findFollowersByUserId(currentUserId, userId, pageable.of());
+        return ResponseEntity.ok().body(new PageImpl<SimpleUserInfoDto>(result));
     }
 
     @PostMapping("/{userId}/follow")
@@ -56,7 +60,7 @@ public class UserController {
         return ResponseEntity.ok().build();
     }
 
-    @DeleteMapping("/{userId}/follows/{targetUserId}")
+    @DeleteMapping("/{userId}/followers/{targetUserId}")
     public ResponseEntity<?> unfollow(@PathVariable("userId") String userId, @PathVariable("targetUserId") String targetUserId){
         followService.follow(userId, targetUserId);
         return ResponseEntity.ok().build();
