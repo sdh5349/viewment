8c9ca831fc68fbcd9c694716b245b63176e98b3b
JaeHyeok <roywogur@naver.com>
2021-02-02 15:05:44 +0900

    S04P13B105-50 [front] feat: search Map 완료

--- frontend/src/components/search/SearchLocation.vue
+++ frontend/src/components/search/SearchLocation.vue
@@ -1,13 +1,48 @@
 <template>
-  <div>{{aa}}</div>
+  <v-row
+    justify="center"
+  >
+    <v-col
+      lg="4"
+      md="4"
+      sm="6"
+    >
+      <v-card
+        class="mx-auto mt-5"
+        flat
+      >
+        <v-list>
+          <v-list-item
+            v-for="(searchedLocation, i) in searchedLocations.slice(0,9)"
+            :key="i"
+            @click="goMap(searchedLocation)"
+          >
+            <v-list-item-avatar>
+              <v-icon>
+                mdi-map-marker
+              </v-icon>
+            </v-list-item-avatar>    
+            <v-list-item-content>
+              <v-list-item-title >{{searchedLocation.place_name}}</v-list-item-title>
+              <v-list-item-title >{{searchedLocation.address_name}}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      aa : ""
+      searchedLocations: "",
     }
+  },
+  mounted() {
+    window.kakao && window.kakao.maps
+      this.addKakaoMapScript();
   },
   props: {
     search : {
@@ -15,12 +50,37 @@ export default {
     }
   },
   methods: {
-    goSelect() {
-      this.aa = this.search
+      addKakaoMapScript() {
+      const script = document.createElement("script");
+      /* global kakao */
+      script.onload = () => kakao.maps.load(this.initMap)
+      script.src =
+        "http://dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=41dd8e1c2fab039d8dbbff2e13e8d5a5";
+      document.head.appendChild(script);
+    },
+    getLocation() {
+      const self = this
+      var places = new kakao.maps.services.Places();
+
+      var callback = function(result, status) {
+        if (status === kakao.maps.services.Status.OK) {
+          self.searchedLocations = result
+          console.log(result);
+        }
+      };
+      places.keywordSearch(this.search, callback);
+    },
+    goMap(searchedLocation) {
+      console.log(searchedLocation)
+      this.$router.push({ name: 'Feed', params: {id: searchedLocation} })
     }
   },
   created() {
-    this.goSelect()
+  },
+  watch: {
+    search: function(res) {
+      this.getLocation(res)
+    }
   },
 }
 </script>
