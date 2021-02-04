a0a39dbe9152300d87284ff09c0f0b2bb8188ace
juheegg <eespacio95@gmail.com>
2021-02-05 00:29:08 +0900

    [back] fix: 댓글에 사용자 정보 추가

--- backend/src/main/java/com/web/curation/dto/reply/RereplyDto.java
+++ backend/src/main/java/com/web/curation/dto/reply/RereplyDto.java
@@ -1,6 +1,7 @@
 package com.web.curation.dto.reply;
 
 import com.web.curation.domain.reply.Rereply;
+import com.web.curation.dto.user.SimpleUserInfoDto;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
@@ -15,11 +16,13 @@ public class RereplyDto {
     private String contents;
     private String wdate;
     private Long parentId;
+    private SimpleUserInfoDto user;
 
     public RereplyDto(Rereply rereply) {
         rereplyId = rereply.getRereplyId();
         userId = rereply.getUser().getId();
         contents = rereply.getContents();
         wdate = rereply.getWdate().toString();
+        user = new SimpleUserInfoDto(rereply.getUser());
     }
 }
