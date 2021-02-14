83b42811a9c1bc6f6110891a403dacba179c3374
juheegg <eespacio95@gmail.com>
2021-02-15 07:54:25 +0900

    [back] fix: 피드에 필요한 게시글 데이터 수정

--- backend/src/main/java/com/web/curation/dto/article/ArticleFeedDto.java
+++ backend/src/main/java/com/web/curation/dto/article/ArticleFeedDto.java
@@ -1,21 +1,19 @@
 package com.web.curation.dto.article;
 
 import com.web.curation.domain.article.Article;
-import com.web.curation.dto.hashtag.HashtagDto;
+import com.web.curation.domain.hashtag.Hashtag;
 import com.web.curation.dto.image.ImageDto;
 import com.web.curation.dto.pin.PinDto;
-import com.web.curation.dto.reply.ReplyDto;
 import com.web.curation.dto.user.SimpleUserInfoDto;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
  * com.web.curation.dto.article
- * ArticleSimpleDto.java
- * @date    2021-02-02 오후 11:16
+ * ArticleFeedDto.java
+ * @date    2021-02-15 오전 7:35
  * @author  이주희
  *
  * @변경이력
@@ -23,16 +21,20 @@ import java.util.List;
 
 @Getter
 @Setter
-public class ArticleSimpleDto {
+public class ArticleFeedDto {
     private Long articleId;
     private SimpleUserInfoDto user;
     private PinDto pin;
     private ImageDto thumbnail;
     private String date;
     private String wdate;
-    private boolean following;
+    private List<Hashtag> hashtags;
+    private double distance;
+    private int likes;
+    private boolean liked;
+    private boolean recommended;
 
-    public ArticleSimpleDto(Article article){
+    public ArticleFeedDto(Article article){
         this.articleId = article.getArticleId();
         this.user = new SimpleUserInfoDto(article.getUser());
         this.pin = new PinDto(article.getPin(), article.getPin().getTrendArticleId());
--- backend/src/main/java/com/web/curation/service/recommend/RecommendService.java
+++ backend/src/main/java/com/web/curation/service/recommend/RecommendService.java
@@ -1,13 +1,12 @@
 package com.web.curation.service.recommend;
 
-import com.web.curation.dto.article.ArticleInfoDto;
-import com.web.curation.dto.article.ArticleSimpleDto;
+import com.web.curation.dto.article.ArticleFeedDto;
 import com.web.curation.dto.user.SimpleUserInfoDto;
 
 import java.util.List;
 
 public interface RecommendService {
 
-    List<ArticleSimpleDto> recommendArticle(String userId);
+    List<ArticleFeedDto> recommendArticle(String userId);
     List<SimpleUserInfoDto> recommendUser(String userId);
 }
