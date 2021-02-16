1e4dc126954f501c44189834f613def6385692ac
jongseong kim <rlawhdtjd9@naver.com>
2021-02-16 23:31:44 +0900

    [back] fix: swagger api에서 response 데이터 명시

--- backend/src/main/java/com/web/curation/controller/notification/NotificationController.java
+++ backend/src/main/java/com/web/curation/controller/notification/NotificationController.java
@@ -31,28 +31,28 @@ public class NotificationController {
 
     @ApiOperation(value = "푸시 알림 정보 조회")
     @GetMapping({"/{userId}"})
-    public ResponseEntity<?> getNotiInfo(@PathVariable("userId") String userId){
+    public ResponseEntity<UserNotiDto> getNotiInfo(@PathVariable("userId") String userId){
         UserNotiDto dto = notificationService.getNotiInfo(userId);
         return ResponseEntity.ok().body(dto);
     }
 
     @ApiOperation(value = "확인하지 않은 알림 개수 조회")
     @GetMapping({"/uncheckcnt/{userId}"})
-    public ResponseEntity<?> getCountUncheckNoti(@PathVariable("userId") String userId){
+    public ResponseEntity<Long> getCountUncheckNoti(@PathVariable("userId") String userId){
         long cnt = notificationService.getCountUncheckNoti(userId);
         return ResponseEntity.ok().body(cnt);
     }
 
     @ApiOperation(value = "확인하지 않은 알림 목록 조회")
     @GetMapping({"/unchecklist/{userId}"})
-    public ResponseEntity<?> getUncheckNoti(@PathVariable("userId") String userId){
+    public ResponseEntity<List<NotiListDto>> getUncheckNoti(@PathVariable("userId") String userId){
         List<NotiListDto> result = notificationService.getUncheckNoti(userId);
         return ResponseEntity.ok().body(result);
     }
 
     @ApiOperation(value = "확인한 알림 목록 조회")
     @GetMapping({"/checklist/{userId}"})
-    public ResponseEntity<?> getcheckNoti(@PathVariable("userId") String userId){
+    public ResponseEntity<List<NotiListDto>> getcheckNoti(@PathVariable("userId") String userId){
         List<NotiListDto> result = notificationService.getcheckNoti(userId);
         return ResponseEntity.ok().body(result);
     }
